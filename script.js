68a6377e826ccbfb69ff0ce336eed2e135120938
Diego Cordoba Jofre <diego@MacBook-Pro-de-Diego.local>
2022-12-13 12:46:56 +0100

    firts training JS

---
+++ script.js
@@ -0,0 +1,64 @@
+//Variables
+//Reserva de memoria, donde guardo informacion. La informacion puede cambiar (en tiempo de ejecucion)
+
+//var
+var name = "Adrian"
+
+let number = 1
+
+//lets
+let pet = "Dog"
+
+//const
+const lastName = "Cordoba";
+
+//-----------------------------------------------------------------------------------------------//
+
+var name = "Diego";
+
+//Incremento y decremento
+number++;
+number--;
+
+
+//concatenacion
+console.info("el nombre del facilitador " +  name + " " + "y tengo un " + pet)
+ 
+//interpolacion
+console.info(`mi nombre es ${name} y tengo un ${pet}`)
+
+//-----------------------------------------------------------------------------------------------//
+
+//Ejercicios//
+//Escribir una oración o texto por consola (que cuente lo que quieran)
+//La oración o texto debe estar compuesto por 3 variables, 3 constantes. Y al menos 3 tipos de datos.
+
+//-----------------------------------------------------------------------------------------------//
+
+//Funciones>
+//Una función es un bloque de instrucciones que puede ejecutarse tantas veces como desee.
+//Una función puede tener parámetros y devolver un valor. Las funciones se pueden definir de diferentes 
+//maneras. Quizás la más común es la declaración de función. Comienza con la palabra clave function. Sigue:
+//El nombre de la función,los parámetros encerrados entre paréntesis, las afirmaciones encerradas entre corchetes.
+
+function color() {
+    return 'red';
+  }
+
+//-----------------------------------------------------------------------------------------------//
+
+//Ejercicios//
+//1
+//Defina una función helloque devuelva 'Hello world!'. 
+//Imprimirla en consola con un string que nos calare que ejercicio es.
+
+//2
+//Defina dos funciones.
+//La primera función adebería devolver 'Hello a!'y la segunda función bdebería devolver 'Hello b!'.
+//Imprimirla en consola con un string que nos calare que ejercicio es.
+
+//3
+//Defina una función greet que devuelva el valor 'Haydo!'.
+//Declarar una variable salutation. 
+//Llame a la función greety asigne el resultado de la llamada a la variable salutation.
+//Imprimirla en consola con un string que nos calare que ejercicio es.
